b6caee99890925908ef3c9e5a4f5cda8ea2f0f32
qwqb233 <qwqb.zhang@gmail.com>
2025-10-05 20:29:09 +0800

    init commit

--- Core/Src/motorLib/Inc/motorClass.h
+++ Core/Src/motorLib/Inc/motorClass.h
@@ -1,3 +1,11 @@
+/*
+ * @Author: qwqb233 qwqb.zhang@gmail.com
+ * @Date: 2025-05-15 20:42:32
+ * @LastEditors: qwqb233 qwqb.zhang@gmail.com
+ * @LastEditTime: 2025-10-05 20:23:53
+ * @FilePath: \course design5\Core\Src\motorLib\Inc\motorClass.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 //
 // Created by qwqb233 on 2025/2/24.
 //
